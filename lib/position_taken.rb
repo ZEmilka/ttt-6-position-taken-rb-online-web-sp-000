0fd398b47d11e3f86dc74682fe1a2349623e0deb
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-31 17:10:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
-# code your #position_taken? method here!
+def position_taken?(board,index)
+end
+#if "#{board}"==0
+#end
+if position_taken==" "
+  #return"false"
+elsif "position_taken"==""
+  #return"false"
+end
